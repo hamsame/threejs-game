9df8e54565009c0e2a6af9d5c04677b87dfcd45a
hamsame <hamsaaelmi@gmail.com>
2023-03-20 20:23:30 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as THREE from "three"
+import "./styles/style.css"
+
+console.log(THREE)
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 5000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(glb|gltf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Three JS Game",
+      heading1: "Cubefield",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+}
