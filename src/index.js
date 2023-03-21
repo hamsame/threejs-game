0bca8d1355b40eb7a58af5c4265bc5446e96d31d
hamsame <hamsaaelmi@gmail.com>
2023-03-21 00:03:23 +0000

    Rendered red cube

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import * as THREE from "three"
 import "./styles/style.css"
 
-console.log(THREE)
+// console.log(THREE)
+// model, scene, camera, renderer
+
+const size = { width: 800, height: 600 }
+const { width, height } = size
+
+// Scene
+const scene = new THREE.Scene()
+
+// cube
+const box = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: "red" })
+const cube = new THREE.Mesh(box, material)
+scene.add(cube)
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, width / height)
+scene.add(camera)
+camera.position.z = 5
+
+// renderer
+const canvas = document.querySelector("canvas")
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+
+renderer.setSize(width, height)
+renderer.render(scene, camera)
