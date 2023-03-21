55e21aed20a18a93b3fef4d41e836a93ee2528ef
hamsame <hamsaaelmi@gmail.com>
2023-03-21 01:54:47 +0000

    added more cubes using a for loop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import * as THREE from "three"
 import "./styles/style.css"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
-// console.log(THREE)
 // model, scene, camera, renderer
-
 const size = { width: 800, height: 600 }
 const { width, height } = size
 
@@ -12,20 +11,41 @@ const scene = new THREE.Scene()
 
 // cube
 const box = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: "red" })
-const cube = new THREE.Mesh(box, material)
-scene.add(cube)
+box.center()
+const material = new THREE.MeshBasicMaterial({
+  color: "blue",
+  wireframe: false,
+})
+for (let i = 0; i < 4; i++) {
+  const cube = new THREE.Mesh(box, material)
+  cube.position.x += i * 2
+  scene.add(cube)
+}
 
 // camera
-const camera = new THREE.PerspectiveCamera(75, width / height)
+const camera = new THREE.PerspectiveCamera(25, width / height)
 scene.add(camera)
-camera.position.z = 5
+camera.position.z = 6
 
 // renderer
 const canvas = document.querySelector("canvas")
 const renderer = new THREE.WebGLRenderer({
   canvas,
 })
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 renderer.setSize(width, height)
-renderer.render(scene, camera)
+
+const tick = () => {
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call func again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
