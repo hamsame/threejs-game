fcf718cbd196dc4fc452666c2d3f90e54d246ba0
hamsame <hamsaaelmi@gmail.com>
2023-03-25 15:03:01 +0000

    Made canvas responsive

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as THREE from "three"
 import "./styles/style.css"
 
-const [width, height] = [window.innerWidth - 90, window.innerHeight - 90]
+const [width, height] = [window.innerWidth * 0.9, window.innerHeight * 0.9]
 
 // scene
 const scene = new THREE.Scene()
@@ -24,6 +24,21 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 })
 
+const resizeCanvas = () => {
+  camera.aspect = width / height
+  renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  camera.updateProjectionMatrix()
+}
+
 renderer.setSize(width, height)
-// renders scene
-renderer.render(scene, camera)
+const loop = () => {
+  window.addEventListener("resize", resizeCanvas)
+
+  // renders scene
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(loop)
+}
+
+loop()
